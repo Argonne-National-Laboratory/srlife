086c94385aa5c28656b6fcd567a98852a7f33db8
Mark Messner <mark.messner@gmail.com>
2020-06-09 15:01:35 -0500

    Working thermal solver, now to speed up a bit

--- test/solvers/test_manufactured.py
+++ test/solvers/test_manufactured.py
@@ -21,8 +21,8 @@ class TestThermal(unittest.TestCase):
           lambda t, r: np.sin(r),
           lambda t, k, alpha, r: k * np.sin(r) - k/r*np.cos(r)),
         ManufacturedSolution(2,
-            lambda t, r, th: t,
-            lambda t, k, alpha, r, th: k/alpha * (r*0.0+1)),
+          lambda t, r, th: t,
+          lambda t, k, alpha, r, th: k/alpha * (r*0.0+1)),
         ManufacturedSolution(2,
           lambda t, r, th: np.sin(r),
           lambda t, k, alpha, r, th: k * np.sin(r) - k/r * np.cos(r)),
@@ -31,20 +31,35 @@ class TestThermal(unittest.TestCase):
           lambda t, k, alpha, r, th: k * np.cos(th) / r**2.0),
         ManufacturedSolution(2,
           lambda t, r, th: np.cos(th) / r,
-          lambda t, k, alpha, r, th: 0.0*r),
+          lambda t, k, alpha, r, th: -k*np.cos(th) / r**3.0),
         ManufacturedSolution(2,
           lambda t, r, th: np.log(r) * np.sin(th) / (t+1),
           lambda t, k, alpha, r, th: k*np.log(r)*np.sin(th)/((t+1)*r**2.0) - k/alpha * np.log(r) * np.sin(th) / (t+1)**2.0),
         ManufacturedSolution(3,
           lambda t, r, th, z: t,
           lambda t, k, alpha, r, th, z: k/alpha * (r*0.0+1)),
+        ManufacturedSolution(3,
+          lambda t, r, th, z: np.sin(r),
+          lambda t, k, alpha, r, th, z: k * np.sin(r) - k/r * np.cos(r)),
+        ManufacturedSolution(3,
+          lambda t, r, th, z: np.cos(th),
+          lambda t, k, alpha, r, th, z: k * np.cos(th) / r**2.0),
+        ManufacturedSolution(3,
+          lambda t, r, th, z: np.sin(z),
+          lambda t, k, alpha, r, th, z: k * np.sin(z)),
+        ManufacturedSolution(3,
+          lambda t, r, th, z: z**2.0*np.cos(th)/r,
+          lambda t, k, alpha, r, th, z: -k*np.cos(th)/r*((z/r)**2.0+2)),
+        ManufacturedSolution(3,
+          lambda t, r, th, z: np.log(r)*np.sin(th)*np.cos(z)/(t+1.0),
+          lambda t, k, alpha, r, th, z: k*np.log(r) * np.sin(th) * np.cos(z) / (t+1) * (1.0 + 1/r**2.0 - 1.0/(alpha*(t+1)))),
         ]
 
     self.solver = thermal.FiniteDifferenceImplicitThermalSolver()
     self.material = materials.ConstantThermalMaterial("Test", 10.0, 5.0)
     self.fluid = materials.ConstantFluidMaterial({"Test": 7.5})
 
-    self.tol = 1e-2
+    self.tol = 1e-4
     self.atol = 1e-1
 
   def _check_case(self, case):
