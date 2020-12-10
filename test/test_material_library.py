cb2cc67f7424ef2bbc0f291a331b8a90bc79f602
Mark Messner <mark.messner@gmail.com>
2020-12-10 16:32:41 -0600

    Hmm, additional incompatibility with whatever NEML I have available on pypi

--- test/test_material_library.py
+++ test/test_material_library.py
@@ -8,14 +8,10 @@ alloys = ['316H','800H','A617','740H','A282','A230']
 class TestMaterials(unittest.TestCase):
   def test_load_materials(self):
     for alloy in alloys:
-      print(alloy)
-      print(1)
       thermal, deformation, damage = library.load_material(alloy, "base","base","base")
       deformation.get_neml_model()
-      print(2)
       thermal, deformation, damage = library.load_material(alloy, "base","elastic_creep","base")
       deformation.get_neml_model()
-      print(3)
       thermal, deformation, damage = library.load_material(alloy, "base","elastic_model","base")
       deformation.get_neml_model()
 
