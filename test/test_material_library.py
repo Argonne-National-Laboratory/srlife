2a0913960daa985d2792e24b22d9c20f73f752d2
Mark Messner <mark.messner@gmail.com>
2020-12-10 13:19:37 -0600

    Fixed test + updated xml syntax

--- test/test_material_library.py
+++ test/test_material_library.py
@@ -1,15 +1,24 @@
+import unittest
+
 from srlife import library
 
 fluids = ['salt']
 alloys = ['316H','800H','A617','740H','A282','A230']
 
-for alloy in alloys:
-    thermal, deformation, damage = library.load_material(alloy, "base","base","base")
-    deformation.get_neml_model()
-    thermal, deformation, damage = library.load_material(alloy, "base","elastic_creep","base")
-    deformation.get_neml_model()
-    thermal, deformation, damage = library.load_material(alloy, "base","elastic_model","base")
-    deformation.get_neml_model()
+class TestMaterials(unittest.TestCase):
+  def test_load_materials(self):
+    for alloy in alloys:
+      print(alloy)
+      print(1)
+      thermal, deformation, damage = library.load_material(alloy, "base","base","base")
+      deformation.get_neml_model()
+      print(2)
+      thermal, deformation, damage = library.load_material(alloy, "base","elastic_creep","base")
+      deformation.get_neml_model()
+      print(3)
+      thermal, deformation, damage = library.load_material(alloy, "base","elastic_model","base")
+      deformation.get_neml_model()
 
-for fluid in fluids:
-    library.load_fluid(fluid,"base")
+    def test_load_fluids(self):
+      for fluid in fluids:
+        library.load_fluid(fluid,"base")
