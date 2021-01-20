9fa8cc440acfebc6ba9fbff47c57e8242304572a
Mark Messner <mark.messner@gmail.com>
2021-01-19 18:11:11 -0600

    Incremented version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup (
     # Name of the project
     name = 'srlife',
     # Version
-    version = '1.0.2',
+    version = '1.0.3',
     # One line-description
     description = "Evaluate the structural life of a solar receiver",
     # README
