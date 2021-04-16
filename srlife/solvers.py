eb4f1a64781ca7c555fd79553a445ea37be4830c
Mark Messner <mark.messner@gmail.com>
2021-04-16 17:11:20 -0500

    Added linesearch to the NR solver

--- srlife/solvers.py
+++ srlife/solvers.py
@@ -6,7 +6,8 @@ import numpy as np
 import numpy.linalg as la
 
 def newton(RJ, x0, rel_tol = 1.0-6, abs_tol = 1.0e-8, miters = 20,
-    linear_solver = la.solve, verbose = True, return_extra = False):
+    linear_solver = la.solve, verbose = True, return_extra = False,
+    linesearch = True, max_search = 10):
   """
     Simple newton-raphson solver
 
@@ -22,6 +23,8 @@ def newton(RJ, x0, rel_tol = 1.0-6, abs_tol = 1.0e-8, miters = 20,
       verbose       if true, print debug info,
       return_extra  if true also return the final residual vector and
                     Jacobian matrix
+      linesearch    if true do backtracking linesearch
+      max_search    max number of backtracking steps
   """
   x = np.copy(x0)
 
@@ -31,17 +34,39 @@ def newton(RJ, x0, rel_tol = 1.0-6, abs_tol = 1.0e-8, miters = 20,
   nR0 = nR
 
   if verbose:
-    print("Iter\tnR\t\tnR/nR0")
-    print("%i\t%3.2e" % (0, nR0))
+    if linesearch:
+      print("Iter\tnR\t\tnR/nR0\t\talpha")
+      print("%i\t%3.2e\t%3.2e" % (0, nR0,1))
+    else:
+      print("Iter\tnR\t\tnR/nR0")
+      print("%i\t%3.2e" % (0, nR0))
 
   for i in range(miters):
     if nR < abs_tol or nR/nR0 < rel_tol:
       break
-    x -= linear_solver(J, R)
-    R, J = RJ(x)    
-    nR = la.norm(R)
+    dx = linear_solver(J,R)
+    if linesearch:
+      alpha = 1.0
+      nR_last = nR
+      x_last = np.copy(x)
+      for i in range(max_search):
+        x = x_last - alpha * dx
+        R, J = RJ(x)
+        nR = la.norm(R)
+        if nR < nR_last:
+          break
+        else:
+          alpha /= 2.0
+    else:
+      x -= dx
+      R, J = RJ(x)    
+      nR = la.norm(R)
+
     if verbose:
-      print("%i\t%3.2e\t%3.2e" % (i+1, nR, nR/nR0))
+      if linesearch:
+        print("%i\t%3.2e\t%3.2e\t%3.2e" % (i+1, nR, nR/nR0,alpha))
+      else:
+        print("%i\t%3.2e\t%3.2e" % (i+1, nR, nR/nR0))
   else:
     raise RuntimeError("Nonlinear solver did not converge!")
 
