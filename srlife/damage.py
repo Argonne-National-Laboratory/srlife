53c06d5319cd97594f5272e9f3f9276508780bb1
William Logie <will.logie@anu.edu.au>
2021-09-28 21:12:44 +0200

    von Mises stress and HBB-T 1413 strain range had typos

--- srlife/damage.py
+++ srlife/damage.py
@@ -1,5 +1,5 @@
 """
-  Module with methods for calculating creep-fatigue damage given 
+  Module with methods for calculating creep-fatigue damage given
   completely-solved tube results and damage material properties
 """
 
@@ -29,13 +29,13 @@ class DamageCalculator:
         tube:       fully-populated tube object
         material:   damage material
         receiver:   receiver object (for metadata)
-    """ 
+    """
     raise NotImplementedError("Superclass not implemented")
 
-  def determine_life(self, receiver, material, nthreads = 1, 
+  def determine_life(self, receiver, material, nthreads = 1,
       decorator = lambda x, n: x):
     """
-      Determine the life of the receiver by calculating individual 
+      Determine the life of the receiver by calculating individual
       material point damage and finding the minimum of all points.
 
       Parameters:
@@ -99,14 +99,14 @@ class TimeFractionInteractionDamage(DamageCalculator):
 
     # Material point cycle fatigue damage
     Df = self.fatigue_damage(tube, material, receiver)
-    
+
     nc = receiver.days
 
     # This is going to be expensive, but I don't see much way around it
-    return min(self.calculate_max_cycles(self.make_extrapolate(c), 
-      self.make_extrapolate(f), material) for c,f in 
+    return min(self.calculate_max_cycles(self.make_extrapolate(c),
+      self.make_extrapolate(f), material) for c,f in
         zip(Dc.reshape(nc,-1).T, Df.reshape(nc,-1).T))
-  
+
   def calculate_max_cycles(self, Dc, Df, material, rep_min = 1, rep_max = 1e6):
     """
       Actually calculate the maximum number of repetitions for a single point
@@ -136,17 +136,17 @@ class TimeFractionInteractionDamage(DamageCalculator):
     """
     # For now just use the von Mises effective stress
     vm = np.sqrt((
-        (tube.quadrature_results['stress_xx'] - tube.quadrature_results['stress_yy'])**2.0 + 
-        (tube.quadrature_results['stress_yy'] - tube.quadrature_results['stress_zz'])**2.0 + 
-        (tube.quadrature_results['stress_zz'] - tube.quadrature_results['stress_xx'])**2.0 + 
-        3.0 * (tube.quadrature_results['stress_xy']**2.0 + 
-          tube.quadrature_results['stress_yz']**2.0 + 
+        (tube.quadrature_results['stress_xx'] - tube.quadrature_results['stress_yy'])**2.0 +
+        (tube.quadrature_results['stress_yy'] - tube.quadrature_results['stress_zz'])**2.0 +
+        (tube.quadrature_results['stress_zz'] - tube.quadrature_results['stress_xx'])**2.0 +
+        6.0 * (tube.quadrature_results['stress_xy']**2.0 +
+          tube.quadrature_results['stress_yz']**2.0 +
           tube.quadrature_results['stress_xz']**2.0))/2.0)
 
     tR = material.time_to_rupture("averageRupture", tube.quadrature_results['temperature'], vm)
     dts = np.diff(tube.times)
     time_dmg = dts[:,np.newaxis,np.newaxis]/tR[1:]
-    
+
     # Break out to cycle damage
     inds = self.id_cycles(tube, receiver)
 
@@ -171,10 +171,10 @@ class TimeFractionInteractionDamage(DamageCalculator):
     strain_names = ['mechanical_strain_xx', 'mechanical_strain_yy', 'mechanical_strain_zz',
         'mechanical_strain_yz', 'mechanical_strain_xz', 'mechanical_strain_xy']
     strain_factors = [1.0,1.0,1.0,np.sqrt(2), np.sqrt(2), np.sqrt(2)]
-    
+
     cycle_dmg =  np.array([self.cycle_fatigue(np.array([ef*tube.quadrature_results[en][
-      inds[i]:inds[i+1]] for 
-      en,ef in zip(strain_names, strain_factors)]), 
+      inds[i]:inds[i+1]] for
+      en,ef in zip(strain_names, strain_factors)]),
       tube.quadrature_results['temperature'][inds[i]:inds[i+1]], material)
       for i in range(receiver.days)])
 
@@ -195,7 +195,7 @@ class TimeFractionInteractionDamage(DamageCalculator):
           " number of days and cycle period!")
 
     return inds
-  
+
   def cycle_fatigue(self, strains, temperatures, material, nu = 0.5):
     """
       Calculate fatigue damage for a single cycle
@@ -217,10 +217,10 @@ class TimeFractionInteractionDamage(DamageCalculator):
         de = strains[:,j] - strains[:,i]
         eq = np.sqrt(2) / (2*(1+nu)) * np.sqrt(
             (de[0] - de[1])**2 + (de[1]-de[2])**2 + (de[2]-de[0])**2.0
-            + 3.0/2.0 * (de[3]**2.0 + de[4]**2.0 + de[5]**6.0)
+            + 3.0/2.0 * (de[3]**2.0 + de[4]**2.0 + de[5]**2.0)
             )
         pt_eranges = np.maximum(pt_eranges, eq)
-    
+
     dmg = np.zeros(pt_eranges.shape)
     # pylint: disable=not-an-iterable
     for ind in np.ndindex(*dmg.shape):
